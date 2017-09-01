ebe40c3c6817ecbeca044a1b759dad24e98ee130
Viza <viktoria.haa@hotmail.com>
2017-09-01 17:16:49 +0200

    Wrote report and fixed some styley bits

--- view/report/report.php
+++ view/report/report.php
@@ -6,7 +6,62 @@
 <p><i>Jag klistrade in min fina favicon här till höger ifall det är svårt att se vad den
 ska föreställa. Det är alltså VH, mina initialer.</i></p>
 
-<p></p>
+<p><b>Jag tänkte skriva lite om designen på min sida innan jag besvarar frågorna.</b></p>
+<p>Bland det första jag fixar på mina hemsidor är de (fula) blåa länkarna som är standard. Jag valde
+en utstickande orange färg på både navbaren och länkarna, så de syns tydligt mot det annars neutrala
+färgtemat som går igenom hela sidan. För ögats skull gjorde jag sidan ljus och enkel i färgerna
+så att det är enkelt att hitta allt och läsa texterna (svart på vitt).</p>
+<p>Min header är gjord i photoshop och jag höll även den enkel med en ljus bakgrund och med mitt namn.</p>
+
+<h3>Gör din egen kunskapsinventering baserat på PHP The Right Way, berätta om dina styrkor och svagheter
+som du vill förstärka under det kommande året.</h3>
+<p>Det jag verkligen vill bli bättre på är just att programmera i objektorienterad PHP, vilket vi lärde
+oss i kursen oophp. Jag läste aldrig klart den kursen så jag ligger lite efter där men det är aldrig
+för sent att lära sig, bara man vill! Objektorientering har alltid varit något jag gillat, då man kan
+göra så mycket mer med klasser och när man väl vet hur allt det där hänger ihop kan man skapa väldigt
+kraftfulla program.</p>
+<p>Det är väldigt svårt för mig att komma på några av mina styrkor, då php faktiskt inte är mitt favoritspråk
+att programmera i (tacka vet jag JavaScript). Om jag får tänka tillbaka på något som jag gillat tidigare
+och som inte gett mig allt för stora problem så får jag nog säga databaser med MySQL. Det gillade jag
+verkligen att arbeta med i htmlphp-kursen och jag tyckte inte det var speciellt svårt att få det att fungera.
+Dock tror jag inte vi kommer ha så mycket med databaser att göra i denna kurs, så jag får helt enkelt försöka
+stärka mig i PHP som programmeringspråk över lag.</p>
+
+<h3>Vilket blev resultatet från din mini-undersökning om vilka ramverk som för närvarande är mest populära
+inom PHP (ange källa var du fann informationen)?</h3>
+<p>Jag kikade på <a href="http://www.archer-soft.com/en/blog/top-7-best-php-frameworks-2017">denna sida</a>,
+som kom upp först när jag googlade "best frameworks for php 2017". De som listades som de sju främsta
+ramverken var följande:</p>
+<ul>
+  <li>CakePHP</li>
+  <li>Yii</li>
+  <li>CodeIgniter</li>
+  <li>Laravel</li>
+  <li>Symfony</li>
+  <li>Phalcon</li>
+  <li>Zend</li>
+</ul>
+
+<h3>Berätta om din syn/erfarenhet generellt kring communities och specifikt communities inom opensource
+och programmeringsdomänen.</h3>
+<p>Om dessa communities inte hade funnits tror jag inte jag hade kunnat klara någon av de kurser jag läst
+hittills... Om jag fastnar på något och googlar mitt problem hittar jag i 99/100 fall alltid ett inlägg
+på stackoverflow där någon haft exakt samma problem. Det finns jättemånga duktiga programmerare där ute
+som flitigt är med och svarar på dessa foruminlägg och hjälper oss mindre kunnande.</p>
+<p>Så jag tycker dessa communities förtjänar sin plats på internet och de har hjälpt mig och många andra
+med att lösa problem enkelt och smidigt.</p>
+
+<h3>Vad tror du om begreppet “en ramverkslös värld” som framfördes i videon?</h3>
+<p>Jag tror inte att vi kommer se en helt ramverkslös värld inom en snar framtid, då man behöver både och
+för att tillfredställa alla. De som inte är speciellt erfarna inom skapandet av hemsidor vill säkert
+ha ett färdigt ramverk att bygga på, medan de riktigt kunniga säkert vill bygga från grunden utan att
+känna sig begränsade av moduler, så som det beskrivs i videon. Jag tror själv att jag kommer gå mot den
+ramverksfria delen i framtiden, just för att man kan skräddarsy mer och välja vilka delar man vill använda.</p>
+
+<h3>Hur gick dina förberedelser inför kommentarssystemet?</h3>
+<p>Just som jag skrev högre upp att vi nog inte kommer behöva använda databaser i denna kurs, tänkte
+jag här att det vore ett perfekt sätt att lagra och publicera kommentarer! Längre än så har jag
+nog inte kommit i mitt planerande men det ska bli en rolig utmaning att fixa ett kommentarssystem.</p>
 
 <h2>Kmom02</h2>
 <p>Redovisningstext kommer snart...</p>
