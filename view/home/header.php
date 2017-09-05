7517e239003076887541ef73cba59584b8f5e421
Viza <viktoria.haa@hotmail.com>
2017-09-05 14:53:49 +0200

    Fixed namespace for class

--- view/home/header.php
+++ view/home/header.php
@@ -6,7 +6,7 @@
     <title><?= $title ?></title>
     <link rel="stylesheet" href="../htdocs/css/style.css">
     <link rel='shortcut icon' href='../htdocs/img/favicon.ico'/>
-    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
     <main>
--- view/navbar1/navbar.php
+++ view/navbar1/navbar.php
@@ -1,4 +1,6 @@
 <?php
+namespace Viza\CNavigation;
+
 $urlHome  = $app->url->create("");
 $urlAbout = $app->url->create("about");
 $urlReport = $app->url->create("report");
@@ -9,16 +11,23 @@ $menu = array(
   'report' => array('text'=>'REPORT', 'url'=>$urlReport),
 );
 
-
-
 class CNavigation
 {
     public static function generateMenu($items, $class)
     {
         $html = "<nav class='$class'>\n";
-        foreach ($items as $item) {
-            $html .= "<a href='{$item['url']}'>{$item['text']}</a>\n";
+
+        foreach ($items as $key => $item) {
+            $basename = str_replace(".php", "", basename($_SERVER['REQUEST_URI']));
+            $selected = ($basename == $key)
+                ? 'selected'
+                : null;
+            $html .= "<a href='{$item['url']}' class='{$selected}'>{$item['text']}</a>\n";
         }
+
+        // foreach ($items as $item) {
+        //     $html .= "<a href='{$item['url']}'>{$item['text']}</a>\n";
+        // }
         $html .= "</nav>\n";
         return $html;
     }
